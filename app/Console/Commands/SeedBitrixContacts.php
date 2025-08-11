4c0e3c66f521125c51ed4b0ec5daaecd3e861efc
谢榕川 <xierongchuan@tutamail.com>
2025-08-11 17:44:23 +0500

    Move seeding test contacts from dbseeder to new command

---
+++ app/Console/Commands/SeedBitrixContacts.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\Bitrix\BitrixWebhookService;
+use Faker\Factory as Faker;
+
+class SeedBitrixContacts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bitrix:seed-contacts {count=50}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Gen test contacts and sent to Bitrix24 API';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $faker = Faker::create('ru_RU');
+        $client = new BitrixWebhookService();
+
+        $contacts = collect(range(1, 50))->map(function ($i) use ($faker, $client) {
+            $firstName  = $faker->firstName;
+            $lastName   = $faker->lastName;
+            $secondName = $faker->middleName;
+
+            $contact = (random_int(0, 1) === 0)
+                ? ['NAME' => $firstName, 'SECOND_NAME' => $secondName, 'LAST_NAME' => $lastName]
+                : ['NAME' => "$firstName $secondName", 'SECOND_NAME' => '', 'LAST_NAME' => $lastName];
+
+            echo $i . '. Adding ' . $contact['NAME'] . "\n";
+
+            $response = $client->addContact($contact['NAME'], $contact['SECOND_NAME'], $contact['LAST_NAME']);
+
+            return $response;
+        });
+
+        unset($contacts);
+
+        return Command::SUCCESS;
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,37 +4,15 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use App\Services\Bitrix\BitrixWebhookService;
-use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's resources.
+     * Seed the application's Database.
      */
     public function run(): void
     {
-        $faker = Faker::create('ru_RU');
-        $client = new BitrixWebhookService();
-
-        $contacts = collect(range(1, 50))->map(function ($i) use ($faker, $client) {
-            $firstName  = $faker->firstName;
-            $lastName   = $faker->lastName;
-            $secondName = $faker->middleName;
-
-            $contact = (random_int(0, 1) === 0)
-                ? ['NAME' => $firstName, 'SECOND_NAME' => $secondName, 'LAST_NAME' => $lastName]
-                : ['NAME' => "$firstName $secondName", 'SECOND_NAME' => '', 'LAST_NAME' => $lastName];
-
-            echo $i . '. Adding ' . $contact['NAME'] . "\n";
-
-            $response = $client->addContact($contact['NAME'], $contact['SECOND_NAME'], $contact['LAST_NAME']);
-
-            return $response;
-        });
-
-        unset($contacts);
+        //
     }
 }
