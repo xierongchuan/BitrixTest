302679b417cc1515edbc14c452a3078efd8897b4
谢榕川 <xierongchuan@tutamail.com>
2025-08-11 17:16:44 +0500

    Add contacts list route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
+use App\Services\Bitrix\BitrixWebhookService;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hii!';
+});
+
+Route::get('/contact/list', function () {
+    $client = new BitrixWebhookService();
+    $response = $client->call(
+        'crm.contact.list',
+        ['filter' => [], 'select' => ['ID','NAME', 'SECOND_NAME', 'LAST_NAME']]
+    );
+    return $response;
 });
