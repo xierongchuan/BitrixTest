748ce19a60f9e8a5cbe228403222e7115d0993d1
谢榕川 <xierongchuan@tutamail.com>
2025-08-13 00:25:51 +0500

    Correct bitrix method call

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use App\Services\Bitrix\BitrixWebhookService;
+use App\Services\Bitrix\BitrixApiService;
 
 Route::get('/', function () {
     return 'Hii!';
 });
 
 Route::get('/contact/list', function () {
-    $client = new BitrixWebhookService();
+    $client = new BitrixApiService();
     $response = $client->call(
         'crm.contact.list',
         ['filter' => [], 'select' => ['ID','NAME', 'SECOND_NAME', 'LAST_NAME']]
