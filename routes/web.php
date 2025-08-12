bc2adcb807e07d215a8d159945ed90058e55b822
谢榕川 <xierongchuan@tutamail.com>
2025-08-13 01:03:11 +0500

    Add PHONE field

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/contact/list', function () {
     $client = new BitrixApiService();
     $response = $client->call(
         'crm.contact.list',
-        ['filter' => [], 'select' => ['ID','NAME', 'SECOND_NAME', 'LAST_NAME']]
+        ['filter' => [], 'select' => ['ID','NAME', 'SECOND_NAME', 'LAST_NAME', 'PHONE']]
     );
     return $response;
 });
